3fe6ea599c417af23def5e82962c6d564f97acb4
JCFlores915 <juanflores915@yahoo.es>
2023-07-03 03:27:44 -0600

    operation of the pokemon count table depending on the alphabet

--- src/components/pokemon/PokemonCount.tsx
+++ src/components/pokemon/PokemonCount.tsx
@@ -1,16 +1,64 @@
 import { box } from "./styles"
 import Table from "../shared/Table"
+import { usePokemonList } from "../../hooks/usePokemonList"
+import { ColumnType } from "antd/es/table"
+import { DataSourceType } from "../../interface/Pokemon.Interface"
 
 const PokemonCount = () => {
+    const { pokemonsList, isLoading } = usePokemonList();
+
+    // Funcion para contar el numero de pokemon por cada letra
+  const getPokemonCountByLetter = () => {
+
+    const letterCountMap: any = {};
+
+    // Cuenta el numero de pokemon por cada letra
+    pokemonsList.forEach((pokemon) => {
+      const firstLetter = pokemon.name.charAt(0).toUpperCase();
+      if (letterCountMap[firstLetter]) {
+        letterCountMap[firstLetter] += 1;
+      } else {
+        letterCountMap[firstLetter] = 1;
+      }
+    });
+
+    // Crear la matriz dataSource para la tabla
+    const dataSource = Object.keys(letterCountMap).map((letter) => ({
+      key: letter,
+      letter,
+      count: letterCountMap[letter],
+    }))
+    .sort((a, b) => a.letter.localeCompare(b.letter));
+
+    return dataSource;
+  };
+
+
+    // Crear las columnas de la tabla Letras del abecedario y cantidad de pokemons
+    const columns:ColumnType<DataSourceType>[]= [
+        {
+            title: 'Letra',
+            dataIndex: 'letter',
+            key: 'letter',
+            align: 'center'
+        },
+        {
+            title: 'Cantidad',
+            dataIndex: 'count',
+            key: 'count',
+            align: 'center'
+        },
+    ]
+
     return (
         <div style={box}>
             <h3>Conteo de Pokemons</h3>
             <hr />
 
             <Table
-                columns={[]}
-                dataSource={[]}
-                loading={false}
+                columns={columns}
+                dataSource={getPokemonCountByLetter()}
+                loading={isLoading}
             />
         </div>
     )
